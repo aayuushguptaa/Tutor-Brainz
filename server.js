c3d3f487d81ac7625216ea3ef8df3620363c7820
aayuushguptaa <ayushkumargupta240@gmail.com>
2025-06-24 00:08:42 +0530

    Fix: Serve home.html from correct frontend path

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Serve home.html for root
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'home.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'home.html'));
 });
 
 
